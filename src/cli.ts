3c04c22fbc71dc2b327ea866056cea3b5a383b42
Johann Schopplich <mail@johannschopplich.com>
2021-11-24 16:10:49 +0100

    chore: remove `-e` shorthand

--- src/cli.ts
+++ src/cli.ts
@@ -9,11 +9,10 @@ async function main() {
 
   cli
     .command("[dir]", "Assets directory")
-    .option("-e, --ext <extension>", "Specify file extensions", {
+    .option("--ext <extension>", "Specify file extensions", {
       default: ["css", "js"],
     })
     .action(async (dir: string, flags) => {
-      console.log(flags.ext);
       await build({
         assetsDir: dir ?? assetsDir,
         extensions: arraify(flags.ext),
